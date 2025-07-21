c969e6a6989632b4790812270ff5e94fbbb4ecd8
Ilvyy <71210840+Ilvyy@users.noreply.github.com>
2025-07-21 21:13:47 +0200

    added styling

--- js/boostai.js
+++ js/boostai.js
@@ -3,11 +3,26 @@ var options = {
     header: {
       title: "Coco",
     },
-    styling: {},
+    styling: {
+      avatarShape: "rounded",
+      avatarUrl:
+        "resources/Mental_health_bloem 1.png/",
+      primaryColor: "#A98AB0",
+      chatBubbles:{
+        userBackgroundColor: "#C6E9EA",
+      },
+      buttons:{
+        backgroundColor: "#6C6D6D",
+      }
+
+    },
     messageFeedback: {
       hide: true,
     },
     settings: {
+      requestFeedback: false,
+      startTriggerActionId: 5281,
+      authStartTriggerActionId: 5281,
       enableProactivityForSmallDevices: true,
       alwaysFullscreen: false,
     },
@@ -16,6 +31,5 @@ var options = {
 var boost = window.boostInit("turesearch", options);
 
 function openChat() {
-  boost.chatPanel.triggerAction(5281);
   boost.chatPanel.show();
 }
