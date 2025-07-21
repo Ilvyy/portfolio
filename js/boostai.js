f1d71e1c32c0396cb4ceeb3caeb80bc75d410e99
Ilvyy <71210840+Ilvyy@users.noreply.github.com>
2025-07-21 20:46:57 +0200

    removed old styling

--- js/boostai.js
+++ js/boostai.js
@@ -3,31 +3,13 @@ var options = {
     header: {
       title: "Coco",
     },
-    styling: {
-      avatarShape: "rounded",
-      avatarUrl:
-        "https://ts3.mm.bing.net/th?id=OIP.LnbIqFPE4dWS11-oWzbBSAHaFB&pid=15.1",
-      panelShape: "rounded",
-      primaryColor: "#FF4700",
-      fontFamily: "arial",
-      pace: "slower",
-      size: "large",
-      chatBubbles: {
-        userBackgroundColor: "#FFA988",
-        vaBackgroundColor: "#E2D7D3",
-      },
-
-      buttons: {
-        backgroundColor: "#FF6023",
-        hoverBackgroundColor: "#FF8558",
-      },
-    },
+    styling: {},
     messageFeedback: {
       hide: true,
     },
     settings: {
       enableProactivityForSmallDevices: true,
-      alwaysFullscreen: true,
+      alwaysFullscreen: false,
     },
   },
 };
